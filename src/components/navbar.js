f819f42c24af3eebe64eeb86c15e841a5aeeeea6
Roshan1115 <roshanbishi7890@gamil.com>
2021-10-04 21:05:32 +0530

    Completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
-import { FaFacebookSquare, FaInstagramSquare,FaLinkedinIn } from 'react-icons/fa';
+import { FaFacebookSquare, FaInstagramSquare,FaLinkedinIn } from 'react-icons/fa'
+import {GiHamburgerMenu} from 'react-icons/gi'
 
-const navbar = () => {
+const Navbar = () => {
+  const [showMediaIcon, setMediaIcon] = useState(false)
   return (
     <>
       <nav className="main-nav">
+        {/* first div  */}
         <div className="logo">
-          <span>R</span>oshan
-          <span>B</span>ishi
+          <span>RoshanBishi</span>
+          {/* <span>Bishi</span> */}
         </div>
-        <div className="menu">
+
+        {/* 2nd div  */}
+        <div className={showMediaIcon ? "mobile-menu-link" : "menu-link"}>
           <ul>
             <li><a href="#" className="home">Home</a></li>
             <li><a href="#" className="about">About</a></li>
@@ -18,22 +23,31 @@ const navbar = () => {
             <li><a href="#" className="contact">Contact</a></li>
           </ul>
         </div>
-        <div className="socialmedia">
-          <ul>
+
+        {/* 3rd div  */}
+        <div className="social-media">
+          <ul className="social-media-desktop" >
             <li>
               <a href="#"><FaFacebookSquare className="facebook"/></a>
             </li>
             <li>
-              <a href="#"><FaInstagramSquare className="insta" /></a>
+              <a href="#"><FaInstagramSquare className="instagram" /></a>
             </li>
             <li>
               <a href="#"><FaLinkedinIn className="linkedin" /></a>
             </li>
           </ul>
+
+          {/* hamburger */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setMediaIcon(!showMediaIcon)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
         </div>
       </nav>
 
-      <section className="hero-sec">
+      <section className="hero-section">
         <p>Welcome to</p>
         <h1>Roshan's site</h1>
       </section>
@@ -41,4 +55,4 @@ const navbar = () => {
   )
 }
 
-export default navbar;
+export default Navbar;
